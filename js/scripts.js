ba4d383ae76e801da9596aac5d6d44c5934bd225
Veronika <vera3@inbox.ru>
2020-01-20 14:35:56 +0200

    changes made

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
-window.dataLayer.array.forEach(function(event){
-    var newEvent = document.createElement('li');
-    newEvent.innerHTML = '<div class="info"><h2 class="title">' event.short_description.fi '</h2></div>';
-    document.getElementById('event-list').appendChild(newEvent);
+window.dataLayer.forEach(function(event) {
+  var newEvent = document.createElement("li");
+  newEvent.innerHTML =
+    '<div class="info"><h2 class="title">' +
+    event.short_description.fi +
+    '</h2><p class="desc">' +
+    event.description.fi +
+    "</p></div>";
+  document.getElementById("event-list").appendChild(newEvent);
 });
